8757b7408bd8bad1a0d0862a4978000aa49d7ecb
phankiet02 <tuankietvn@outlook.com>
2020-08-18 11:27:49 +0700

    add routing

---
+++ src/Routes/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+export default Home;
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h1> PKcoin </h1>
+
+      </header>
+    </div>
+  );
+}
